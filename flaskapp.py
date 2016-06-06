65c0869b73df92d61f8663a496bb8b9b5931ac80
Ubuntu <ubuntu@ip-172-31-35-224.ec2.internal>
2016-06-06 00:07:34 +0000

    Add server code to specify an activity from the Android app so that we can periodically poll this from the HoloLens

--- flaskapp.py
+++ flaskapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello from Flask!'
+	return 'This app is used for ProjectLive, as CS3377M project!'
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -21,6 +21,22 @@ def show_post(post_id):
 def get_hr():
 	ret_str = ""
 	txt = open('/hr/hr.txt', 'r')
+	ret_str = txt.read()
+	txt.close()
+	return ret_str
+
+@app.route('/change_activity/<int:post_id>')
+def change_activity(post_id):
+	# show the post with the given id, the id is an integer
+	activity = '%d' % post_id
+	with open('/hr/activity.txt', 'w') as f:
+		f.write(activity)
+	return '%d' % post_id
+
+@app.route('/get_activity')
+def get_activity():
+	ret_str = ""
+	txt = open('/hr/activity.txt', 'r')
 	ret_str = txt.read()
 	txt.close()
 	return ret_str
